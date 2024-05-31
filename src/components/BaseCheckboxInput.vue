3d86b327df4747e1baf77e8d1841c04c318074f2
thiagowehren <thiago-ws@hotmail.com>
2024-05-31 03:25:11 -0300

    add base input components

---
+++ src/components/BaseCheckboxInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="input-wrap">
+    <label v-if="label">{{ label }}</label><br/>
+    <input
+      :type="type"
+      :value="modelValue"
+      @change="$emit('update:modelValue', $event.target.checked)"
+      v-bind="$attrs"
+    /><br/>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: [String, Boolean],
+    default: false,
+  },
+  modelValue: {
+    type: [String, Boolean],
+    default: "",
+  },
+});
+</script>
---
+++ src/components/BaseTextInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="input-wrap">
+    <label v-if="label">{{ label }}</label><br/>
+    <input
+      :type="type"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      v-bind="$attrs"
+    /><br/>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  type: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: [String, Boolean],
+    default: false,
+  },
+  modelValue: {
+    type: String,
+    default: "",
+  },
+});
+</script>
