0a150a28284399da3c4b163e4670cb002aac8d55
thiagowehren <thiago-ws@hotmail.com>
2024-06-11 04:49:08 -0300

    add form error messages

---
+++ src/helpers/constants/productFormErrorMessages.ts
@@ -0,0 +1,6 @@
+export const productFormErrorMessages = {
+  name: {
+    required: "Product title is required",
+    invalid: "Title is too short (minimum: 1 characters)"
+  }
+};
---
+++ src/helpers/constants/storeFormErrorMessages.ts
@@ -0,0 +1,6 @@
+export const storeFormErrorMessages = {
+  name: {
+    required: "Store name is required",
+    invalid: "Name is too short (minimum: 3 characters)"
+  }
+};
