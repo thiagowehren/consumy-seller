0da1e69542b74d9b0d9e8082fb05f1ea11c2444e
thiagowehren <thiago-ws@hotmail.com>
2024-06-11 16:44:53 -0300

    refactor StoreDetails to use new ProductDetails component for displaying products and improving readabilty

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { ProductResponse } from '@/dtos/productDTO';
+import defaultProductImage from '@/assets/dish-default-256.png'; 
+
+const productInfo = ref<ProductResponse | null>(null);
+
+const props = defineProps<{
+  product: ProductResponse | null;
+  storeId: string;
+}>();
+
+watch(() => props.product, (newValue) => {
+  productInfo.value = newValue;
+});
+</script>
+
+<template>
+  <div>
+    <h2>Product Details</h2>
+    <div v-if="product">
+      <img :src="product.image_url ? product.image_url : defaultProductImage" alt="Imagem do Produto" style="width: 100px; height: 100px;">
+      <p>Title: {{ product.title }}</p>
+      <p>Price: {{ product.price }}</p>
+      <p v-if="product.hidden">Hidden: {{ product.hidden }}</p>
+      <router-link :to="'/stores/' + storeId + '/products/' + product.id + '/edit'">Editar</router-link>
+    </div>
+  </div>
+</template>
--- src/components/StoreDetails.vue
+++ src/components/StoreDetails.vue
@@ -2,17 +2,22 @@
 import { ref, onMounted } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import { getStore } from '@/api/storeAPI';
+import { getAllStoreProducts } from '@/api/productAPI';
 import { standardizeErrorMessage } from '@/helpers/standardizeErrorMessage';
 import defaultShopImage from '@/assets/shop-default-256.png';
-import { StoreResponse } from '@/dtos/storeDTO';
+import { StoreResponse, ProductResponse } from '@/dtos/storeDTO';
 import LinkPathNav from '@/components/LinkPathNav.vue';
+import ProductDetails from '@/components/ProductDetails.vue';
 
 const route = useRoute();
 const router = useRouter();
 
 const errorMessage = ref('');
-const store = ref<StoreResponse>();
-const storeId = route.params.storeId as string
+const store = ref<StoreResponse | null>(null);
+const products = ref<ProductResponse[]>([]);
+const storeId = route.params.storeId as string;
+
+const isStoreLoaded = ref(false);
 
 onMounted(() => {
   fetchStore();
@@ -22,12 +27,27 @@ const fetchStore = async () => {
   try {
     const response = await getStore(storeId);
     store.value = response;
+    isStoreLoaded.value = true;
+    fetchProducts();
   } catch (error) {
     errorMessage.value = standardizeErrorMessage(error);
     console.log(errorMessage.value);
   }
 };
 
+const fetchProducts = async () => {
+  if (!isStoreLoaded.value) {
+    return;
+  }
+
+  try {
+    const response = await getAllStoreProducts(storeId);
+    products.value = response.products;
+  } catch (error) {
+    errorMessage.value = standardizeErrorMessage(error);
+    console.log(errorMessage.value);
+  }
+};
 </script>
 
 <template>
@@ -36,12 +56,20 @@ const fetchStore = async () => {
       <h1>Store</h1>
 
       <div><p v-if="errorMessage">{{ errorMessage }}</p></div>
-      <div><LinkPathNav :route="`stores/${storeId}`" /></div>
+      <div><LinkPathNav :route="`stores/${storeId}`" :clickableSegments="[0, 1]" /></div>
       <div v-if="store">
           <img :src="store.image_url ? store.image_url : defaultShopImage" alt="Imagem da loja" style="width: 100px; height: 100px;">
           <p>{{ store.name }}</p>
           <p>Hidden: {{store.hidden}}</p>
           <router-link :to="'/stores/' + store.id + '/edit'">Editar</router-link>
+      </div>
+
+      <h2>Products</h2>
+      <div v-if="products.length > 0">
+        <ProductDetails v-for="product in products" :key="product.id" :product="product" :storeId="storeId" />
+      </div>
+      <div v-else>
+        <p>No products available</p>
       </div>
     </div>
   </main>
