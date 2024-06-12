0a92b4913b74a979e07dd8d7834ec8b80def4fbc
thiagowehren <thiago-ws@hotmail.com>
2024-06-12 03:34:07 -0300

    refactor standardizeErrorMessage to handle non-HTTP errors

--- src/helpers/standardizeErrorMessage.ts
+++ src/helpers/standardizeErrorMessage.ts
@@ -5,20 +5,15 @@ export const standardizeErrorMessage = (error: Error): string => {
   if (error instanceof HTTPError) {
     switch (error.status) {
         case 401:
-            // console.error('User is not authorized to access this resource.');
             return APIErrorMessages.standard.unauthorized;
         case 404:
-            // console.error('The requested resource was not found.');
             return APIErrorMessages.standard.notFound;
         case 422:
-            // console.error('The requested is unprocessable.');
             return APIErrorMessages.standard.unprocessable;
         default:
-            // console.error('An unexpected HTTP Error occurred:', error);
-            return `An unexpected HTTP Error occurred! STATUS ${error.status}`;
+            return `[default] An unexpected HTTP Error occurred! STATUS ${error.status}`;
     }
   } else {
-    // console.error('An unexpected error occurred:', error);
-    return error.toString();
+    return `[default] An unexpected non-HTTP error occurred: ${error.toString()}`;
   }
 };
