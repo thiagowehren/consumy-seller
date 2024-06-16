4a39a8802321748b566eb7c56a75c8a2c550d734
thiagowehren <thiago-ws@hotmail.com>
2024-06-16 00:56:36 -0300

    revamp welcome page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
-import UserProfile from '@/components/UserProfile.vue'
 </script>
 
 <template>
   <v-app>
     <v-main>
-      <UserProfile />
+      <TheWelcome />
     </v-main>
   </v-app>
 </template>
