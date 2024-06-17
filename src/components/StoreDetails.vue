9c7b67937057b6d3049a05ffd7bfc711946d5d4a
thiagowehren <thiago-ws@hotmail.com>
2024-06-17 16:25:03 -0300

    revamp StoreDetails and StoreList components

--- src/components/StoreDetails.vue
+++ src/components/StoreDetails.vue
@@ -1,13 +1,124 @@
+<template>
+  <v-app>
+    <v-main>
+      <UserNavigation />
+
+      <div>
+        <div><LinkPathNav :route="`stores/${storeId}`" :clickableSegments="[0, 1]" /></div>
+        <div class="ml-6">
+          <h1 class="text-3xl font-bold">Store</h1>
+        </div>
+        <div v-if="store">
+          <v-card class="mx-auto" max-width="400">
+              <v-img
+                :src="store.image_url ? store.image_url : defaultShopImage"
+                height="200"
+                class="text-white"
+                cover
+                gradient="to bottom, rgba(0,0,0,.5), rgba(0,0,0,.5)"
+              >
+                <v-card-title>
+                  <v-icon v-if="store.hidden">mdi-eye-off</v-icon>
+                  {{ store.name }}
+                </v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pt-4">
+                Created at: {{ store.created_at }}
+              </v-card-subtitle>
+
+              <v-card-actions>
+                <router-link :to="{ path: '/stores/' + store.id }">
+                  <v-btn color="blue">Voltar</v-btn>
+                </router-link>
+                <router-link :to="{ path: '/stores/' + store.id + '/edit' }">
+                  <v-btn color="orange">Editar</v-btn>
+                </router-link>
+              </v-card-actions>
+            </v-card>
+        </div>
+        <div class="ml-6 mt-4">
+          <h2 class="text-3xl font-bold ">Products</h2>
+          <router-link :to=" { path: `/stores/${storeId}/products/new`}">
+            <v-btn class="bg-red-accent-4 mt-4" prepend-icon="mdi-plus">
+              Novo Produto
+            </v-btn>
+          </router-link>
+        </div>
+        <div v-if="products.length > 0">
+          <v-row class="mt-5 ml-5 mr-5">
+            <v-col v-for="product in products" :key="product.id" cols="12" sm="6" md="4" lg="3">
+              <v-card class="mx-auto" max-width="400">
+                <v-img
+                  :src="product.image_url ? product.image_url : defaultShopImage"
+                  height="200"
+                  class="text-white"
+                  cover
+                  gradient="to bottom, rgba(0,0,0,.5), rgba(0,0,0,.5)"
+                >
+                  <v-card-title>
+                  <v-icon v-if="product.hidden">mdi-eye-off</v-icon>
+                    {{ product.title }}
+                  </v-card-title>
+                </v-img>
+
+                <v-card-subtitle class="pt-4">
+                  #{{ product.id }}
+                </v-card-subtitle>
+                <v-card-text>
+                  <div>{{product.expires_at ? `countdown:${product.expires_at}` : ''}}</div>
+
+                  <div v-if="product.price" class="font-bold">Preço: {{product.price}}</div>
+                </v-card-text>
+                <v-card-actions>
+                  <router-link :to="{ path: `/stores/${storeId}/products/${product.id}/edit` }">
+                    <v-btn color="orange">Editar</v-btn>
+                  </router-link>
+                  <v-btn color="red" @click="openDeleteModal(product)">Deletar</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+          <div v-if="isStoreLoaded  && pagination.next" class="text-center m-6">
+            <v-btn color="red-accent-4" @click="fetchMoreProducts">Carregar mais produtos</v-btn>
+          </div>
+          <v-dialog v-model="dialogDelete" max-width="500">
+            <v-card>
+              <v-card-title class="headline">
+                Confirmar Exclusão
+              </v-card-title>
+              <v-card-text>
+                Tem certeza que deseja deletar o produto "{{ selectedProduct.title }}"? É uma ação permanente!
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="dialogDelete = false">
+                  Cancelar
+                </v-btn>
+                <v-btn color="red darken-1" text @click="confirmDelete">
+                  Deletar
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+        <div v-else>
+          <p>Não há produtos disponíveis.</p>
+        </div>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import { getStore } from '@/api/storeAPI';
-import { getAllStoreProducts } from '@/api/productAPI';
+import { getAllStoreProducts, deleteStoreProduct } from '@/api/productAPI';
 import { standardizeErrorMessage } from '@/helpers/standardizeErrorMessage';
 import defaultShopImage from '@/assets/shop-default-256.png';
 import { StoreResponse, ProductResponse, Pagination } from '@/dtos/storeDTO';
 import LinkPathNav from '@/components/LinkPathNav.vue';
-import ProductDetails from '@/components/ProductDetails.vue';
 import UserNavigation from '@/components/UserNavigation.vue';
 
 const route = useRoute();
@@ -20,6 +131,8 @@ const storeId = route.params.storeId as string;
 const pagination = ref<Pagination>({ current: 1, pages: 1 });
 
 const isStoreLoaded = ref(false);
+const dialogDelete = ref(false);
+const selectedProduct = ref<ProductResponse | null>(null);
 
 onMounted(() => {
   fetchStore();
@@ -66,33 +179,19 @@ const fetchMoreProducts = async () => {
     console.log(errorMessage.value);
   }
 };
+
+const openDeleteModal = (product: ProductResponse) => {
+  selectedProduct.value = product;
+  dialogDelete.value = true;
+};
+
+const confirmDelete = async () => {
+  try {
+    await deleteStoreProduct(storeId, selectedProduct.value.id);
+    fetchProducts()
+    dialogDelete.value = false;
+  } catch (error) {
+    // errorMessage.value = standardizeErrorMessage(error);
+  }
+};
 </script>
-
-<template>
-  <v-app>
-    <v-main>
-      <UserNavigation />
-
-      <div>
-        <h1>Store</h1>
-        <div><p v-if="errorMessage">{{ errorMessage }}</p></div>
-        <div><LinkPathNav :route="`stores/${storeId}`" :clickableSegments="[0, 1]" /></div>
-        <div v-if="store">
-            <img :src="store.image_url ? store.image_url : defaultShopImage" alt="Imagem da loja" style="width: 100px; height: 100px;">
-            <p>{{ store.name }}</p>
-            <p>Hidden: {{store.hidden}}</p>
-            <router-link :to="'/stores/' + store.id + '/edit'">Editar</router-link>
-        </div>
-        <h2>Products</h2>
-        <router-link :to="`/stores/${storeId}/products/new`">Create a new product</router-link>
-        <div v-if="products.length > 0">
-          <ProductDetails v-for="product in products" :key="product.id" :product="product" :storeId="storeId" />
-          <button @click="fetchMoreProducts" v-if="pagination.next">Carregar Mais</button>
-        </div>
-        <div v-else>
-          <p>No products available</p>
-        </div>
-      </div>
-    </v-main>
-  </v-app>
-</template>
--- src/components/StoresList.vue
+++ src/components/StoresList.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
-import { getAllStores } from '@/api/storeAPI';
+import { getAllStores, deleteStore } from '@/api/storeAPI';
 import { standardizeErrorMessage } from '@/helpers/standardizeErrorMessage';
 import Paginator from '@/components/PaginatorNav.vue';
 import defaultShopImage from '@/assets/shop-default-256.png';
@@ -24,6 +24,9 @@ if (typeof route.query.page === 'string' && !isNaN(parseInt(route.query.page))) 
 
 const stores = ref<StoreResponse[]>([]);
 const pagination = ref<Pagination>({ current: 1, pages: 1 });
+const contentLoaded = ref(false);
+const dialogDelete = ref(false);
+const selectedStore = ref(null);
 
 onMounted(() => {
   fetchStores();
@@ -40,6 +43,7 @@ const fetchStores = async (newPage?: number) => {
     const response = await getAllStores(page);
     stores.value = response.stores;
     pagination.value = response.pagination;
+    contentLoaded.value = true;
     console.log(pagination.value);
   } catch (error) {
     errorMessage.value = standardizeErrorMessage(error);
@@ -47,33 +51,114 @@ const fetchStores = async (newPage?: number) => {
   }
 };
 
+const openDeleteModal = (store) => {
+  selectedStore.value = store;
+  dialogDelete.value = true;
+};
+
+const confirmDelete = async () => {
+  try {
+    const response = await deleteStore(selectedStore.value.id);
+    dialogDelete.value = false;
+    fetchStores();
+  } catch (error) {
+    // console.error('Erro ao deletar a loja:', error);
+  }
+};
 </script>
 
 <template>
   <v-app>
     <v-main>
       <UserNavigation />
-  
-      <div><LinkPathNav :route="`/stores`" :clickableSegments="[0]"/></div>
+
       <div>
-        <div><p v-if="alert">{{ alert }}</p></div>
-        <div><p v-if="notice">{{ notice }}</p></div>
-        <div><p v-if="errorMessage">{{ errorMessage }}</p></div>
-        <h1>Stores</h1>
-        <router-link to="/stores/new">Create a new store</router-link>
-  
+        <LinkPathNav :route="`/stores`" :clickableSegments="[0]" />
+      </div>
+      <div>
+        <div class="ml-6">
+          <h1 class="text-3xl font-bold ">Stores</h1>
+          <router-link to="/stores/new">
+            <v-btn class="bg-red-accent-4 mt-4" prepend-icon="mdi-plus">
+              nova loja
+            </v-btn>
+          </router-link>
+        </div>
+
         <Paginator :pagination="pagination" path="/stores" @page-change="fetchStores" />
-  
-        <ul v-if="stores.length > 0">
-          <li v-for="store in stores" :key="store.id">
-            <img :src="store.image_url ? store.image_url : defaultShopImage" alt="Imagem da loja" style="width: 100px; height: 100px;">
-            <p>{{ store.name }}</p>
-            <router-link :to="{ path: '/stores/' + store.id }">Ir a loja</router-link>
-            <p v-if="store.hidden">Hidden</p>
-          </li>
-        </ul>
-        <p v-else>Não há lojas disponíveis no momento.</p>
+
+        <v-row v-if="contentLoaded && stores.length > 0" class="mt-5 ml-5 mr-5">
+          <v-col v-for="store in stores" :key="store.id" cols="12" sm="6" md="4" lg="3">
+            <v-card class="mx-auto" max-width="400">
+              <v-img
+                :src="store.image_url ? store.image_url : defaultShopImage"
+                height="200"
+                class="text-white"
+                cover
+                gradient="to bottom, rgba(0,0,0,.5), rgba(0,0,0,.5)"
+              >
+                <v-card-title>
+                  <v-icon v-if="store.hidden">mdi-eye-off</v-icon>
+                  {{ store.name }}
+                </v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pt-4">
+                Created at: {{ store.created_at }}
+              </v-card-subtitle>
+
+              <v-card-actions>
+                <router-link :to="{ path: '/stores/' + store.id }">
+                  <v-btn color="blue">Mostrar</v-btn>
+                </router-link>
+                <router-link :to="{ path: '/stores/' + store.id + '/edit' }">
+                  <v-btn color="orange">Editar</v-btn>
+                </router-link>
+                <v-btn color="red" @click="openDeleteModal(store)">Deletar</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <div v-else-if="contentLoaded && stores.length === 0" class="ml-6">
+          <p>Não há lojas disponíveis no momento.</p>
+        </div>
+
+        <div v-else class="ml-6">
+          <p>Aguarde enquanto carregamos as lojas...</p>
+        </div>
+
+        <v-dialog v-model="dialogDelete" max-width="500">
+          <v-card>
+            <v-card-title class="headline">
+              Confirmar Exclusão
+            </v-card-title>
+            <v-card-text>
+              Tem certeza que deseja deletar a loja "{{ selectedStore.name }}"? É uma ação permanente!
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="dialogDelete = false">
+                Cancelar
+              </v-btn>
+              <v-btn color="red darken-1" text @click="confirmDelete">
+                Deletar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
       </div>
     </v-main>
   </v-app>
 </template>
+
+<style scoped>
+.v-card-title {
+  font-size: 24px;
+  font-weight: bold;
+}
+.v-card-subtitle {
+  font-size: 14px;
+}
+</style>
