2945051f0beddbc49bb93a19a5e9f9c36715b1ec
Thiago Wehren <thiago-ws@hotmail.com>
2024-06-20 09:42:46 -0300

    add login cypress test

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,23 +1,40 @@
 class RegisterForm {
   elements = {
     emailInput: () => cy.get('#input-1'),
-    passwordInput: () => cy.get('#input-2')
+    passwordInput: () => cy.get('#input-3')
   }
 }
 
 describe('Login', () => {
-  describe('Submitting correct login valid inputs', () => {
-    const input = {
-      email:'seller1@example.com',
-      password:'123456'
-    }
-    it('Given I am on the registration page Login', () => {
-      cy.visit('/login')
-    })
+  const registerForm = new RegisterForm();
 
-    it(`when i enter email`)
-    it(`then i enter password`)
-    it(`then i click on the submit button`)
-    it(`then i `)
-  })
-})
+  describe('Submitting correct login with valid inputs', () => {
+    const input = {
+      emailPrefix: 'seller1',
+      emailDomain: 'example.com',
+      password: '123456'
+    };
+
+    it('should visit the login page', () => {
+      cy.visit('/login');
+    });
+
+    it('should fill in the email', () => {
+      registerForm.elements.emailInput().type(input.emailPrefix, { delay: 100 });
+    });
+
+    it('should fill in the password', () => {
+      registerForm.elements.passwordInput().type(input.password, { delay: 100 });
+    });
+
+    it('should submit the login form', () => {
+      cy.get('form').submit();
+    });
+
+    it('should handle incorrect login and correct email domain', () => {
+      registerForm.elements.emailInput().clear().type(`${input.emailPrefix}@${input.emailDomain}`, { delay: 100 });
+      cy.get('form').submit();
+      cy.url().should('eq', 'http://localhost:5173/');
+    });
+  });
+});
